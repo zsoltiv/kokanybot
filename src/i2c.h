3aebd7b7ca96b6d07516e640c64e090cb74c14f7
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2023-01-29 21:59:09 +0000

    PCA9685 register cimzo macro

--- src/i2c.h
+++ src/i2c.h
@@ -5,6 +5,16 @@
 extern "C" {
 #endif
 
+#include <stdint.h>
+
+/* 
+ * PCA9685 relevans registerei
+ *
+ * https://pdf1.alldatasheet.com/datasheet-pdf/view/293576/NXP/PCA9685.html
+ */
+
+#define SERVO(n) ((uint8_t) ((n) * 4 + 6)) // 0x6-nal kezdodnek, 4 octet kulonbseg van a LEDn_ON_LOW-ok kozott
+
 void i2c_init(void);
 void i2c_cleanup(void);
 
