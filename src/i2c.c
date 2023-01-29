6991abe0bd938aef52a6f2bd43ca58fa27e7ebc2
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2023-01-29 23:15:42 +0000

    servo makrobol static inline fuggveny

--- src/i2c.c
+++ src/i2c.c
@@ -7,9 +7,20 @@
 
 #include "i2c.h"
 
+/*
+ * PCA9685 datasheet
+ * https://pdf1.alldatasheet.com/datasheet-pdf/view/293576/NXP/PCA9685.html
+ */
+
 #define PCA9685_ADDR 0x40
 
 int i2c;
+
+static inline uint8_t servo(int n)
+{
+    // 0x6-nal kezdodnek, 4 octet kulonbseg van a LEDn_ON_LOW-ok kozott
+    return (uint8_t) (n * 4 + 6);
+}
 
 void i2c_init(void)
 {
@@ -30,4 +41,15 @@ void i2c_init(void)
 void i2c_cleanup(void)
 {
     close(i2c);
+}
+
+void i2c_servo_set(int n, int degrees)
+{
+    if(n < 0 || n > 15)
+        return;
+    if(degrees < 0 || degrees > 359)
+        return;
+
+    uint8_t addr = servo(n);
+    write(i2c, &addr, 1);
 }
