ba910f245e891254b67ce2cb54514e67532ab007
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2023-03-27 22:14:48 +0200

    servo: Servo thread
    Since the input subsystem only calls functions whenever the state of a
    key changes, we use a different thread to keep the servos moving while a
    key is pressed.

--- src/servo.c
+++ src/servo.c
@@ -5,13 +5,21 @@
 #include "pca9685.h"
 #include "servo.h"
 
+struct servo_thread {
+    struct servo *servos[ARM_SERVO_COUNT];
+    thrd_t thread;
+    mtx_t lock;
+    int change_pin;
+    enum servo_direction change_direction;
+};
+
 struct servo {
     int pin;
     int degrees;
     enum servo_direction direction;
 };
 
-struct servo *servo_init(int pin, int degrees)
+static struct servo *servo_init(int pin, int degrees)
 {
     struct servo *s = malloc(sizeof(struct servo));
     s->pin = pin;
@@ -21,19 +29,72 @@ struct servo *servo_init(int pin, int degrees)
     return s;
 }
 
-void servo_move(struct servo *s, enum servo_direction dir)
+static struct servo *servo_thread_find_change_servo(const struct servo_thread *st)
 {
-    switch(dir) {
-        case SERVO_DIRECTION_FORWARD:
-            if(s->degrees < 179)
-                s->degrees++;
+    for(int i = 0; i < ARM_SERVO_COUNT; i++)
+        if(st->servos[i]->pin == st->change_pin)
+            return st->servos[i];
+    return NULL;
+}
+
+static void servo_step(struct servo *s)
+{
+    switch(s->direction) {
+        case SERVO_DIRECTION_NONE:
             break;
         case SERVO_DIRECTION_BACKWARD:
             if(s->degrees > 0)
                 s->degrees--;
             break;
-        default:
+        case SERVO_DIRECTION_FORWARD:
+            if(s->degrees < 179)
+                s->degrees++;
             break;
     }
+
     pca9685_pin_set(s->pin, s->degrees);
+}
+
+static int servo_thread(void *arg)
+{
+    struct servo_thread *st = (struct servo_thread *) arg;
+
+    while(1) {
+        mtx_lock(&st->lock);
+
+        if(st->change_pin > -1) {
+            struct servo *s = servo_thread_find_change_servo(st);
+            s->direction = st->change_direction;
+
+            st->change_pin = -1;
+        }
+
+        mtx_unlock(&st->lock);
+
+        for(int i = 0; i < ARM_SERVO_COUNT; i++)
+            servo_step(st->servos[i]);
+    }
+}
+
+struct servo_thread *servo_thread_init(int servo_pins[static ARM_SERVO_COUNT])
+{
+    struct servo_thread *st = malloc(sizeof(struct servo_thread));
+    mtx_init(&st->lock, mtx_plain);
+    st->change_direction = SERVO_DIRECTION_NONE;
+    st->change_pin = -1;
+    for(int i = 0; i < ARM_SERVO_COUNT; i++)
+        st->servos[i] = servo_init(servo_pins[i], 90);
+    thrd_create(&st->thread, servo_thread, (void *) st);
+
+    return st;
+}
+
+void servo_thread_change(struct servo_thread *st, int pin, enum servo_direction dir)
+{
+    mtx_lock(&st->lock);
+
+    st->change_pin = pin;
+    st->change_direction = dir;
+
+    mtx_unlock(&st->lock);
 }
--- src/servo.h
+++ src/servo.h
@@ -3,15 +3,16 @@
 
 #include <stdbool.h>
 
+#define ARM_SERVO_COUNT 7
+
 enum servo_direction {
     SERVO_DIRECTION_NONE = 0,
     SERVO_DIRECTION_FORWARD,
     SERVO_DIRECTION_BACKWARD,
 };
 
-struct servo;
+struct servo_thread;
 
-struct servo *servo_init(int pin, int degrees);
-void servo_move(struct servo *s, enum servo_direction dir);
+struct servo_thread *servo_thread_init(int servo_pins[static ARM_SERVO_COUNT]);
 
 #endif /* SERVO_H */
