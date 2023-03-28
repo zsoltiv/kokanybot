2c241dfb7530bfdf3657cd707f4d04c611aa4054
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2023-03-28 13:52:34 +0200

    main: testing code, `stop` function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <threads.h>
+#include <time.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
@@ -10,6 +11,13 @@
 #include "pca9685.h"
 #include "gpio.h"
 #include "input.h"
+#include "pwm.h"
+
+void stop(bool unused)
+{
+    dispatch_pending_events();
+    motor_stop(unused);
+}
 
 struct key_bind key_binds[6] = {
     { .key =     KEY_W, .func =        motor_forward },
@@ -17,7 +25,7 @@ struct key_bind key_binds[6] = {
     { .key =     KEY_S, .func =       motor_backward },
     { .key =     KEY_D, .func =          motor_right },
     { .key = KEY_SPACE, .func = do_image_recognition },
-    { .key =     KEY_R, .func =           motor_stop },
+    { .key =     KEY_R, .func =                 stop },
 };
 
 int main(void)
@@ -31,9 +39,28 @@ int main(void)
     printf("Up and running\n");
     while(1) { // robot loop
         //input_receive_input();
-        for(int i = 0; i < 360; i++) {
-            pca9685_pin_set(0, i);
-        }
+
+        //for(int i = 0; i < 180; i++) {
+        //    pca9685_pin_set(15, i);
+        //    usleep(1000);
+        //}
+        //for(int i = 179; i >= 0; i--) {
+        //    pca9685_pin_set(15, i);
+        //    usleep(1000);
+        //}
+
+        pwm_set_duty_cycle(r, 255);
+        pwm_set_duty_cycle(g, 255);
+        pwm_set_duty_cycle(b, 0);
+        sleep(1);
+        pwm_set_duty_cycle(r, 0);
+        pwm_set_duty_cycle(g, 255);
+        pwm_set_duty_cycle(b, 255);
+        sleep(1);
+        pwm_set_duty_cycle(r, 255);
+        pwm_set_duty_cycle(g, 255);
+        pwm_set_duty_cycle(b, 255);
+        sleep(1);
     }
 
     thrd_join(img_thrd, NULL);
