ef328ba9b2e713656ba09538ad2cc0525096df2a
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2023-06-23 14:44:08 +0200

    main: Update it

--- src/main.c
+++ src/main.c
@@ -10,70 +10,27 @@
 #include <linux/input-event-codes.h>
 
 #include "net.h"
-#include "stream.h"
-#include "pca9685.h"
 #include "gpio.h"
 #include "i2c.h"
 #include "motor.h"
 #include "input.h"
-#include "servo.h"
-
-int servo_pins[] = {11, 12, 13, 14, 15};
-
-void stop(bool unused)
-{
-    motor_stop(unused);
-    for(int i = 0; i < 16; i++)
-        servo_thread_default(sth, i);
-}
-
-static int pin = 15;
-
-#define SERVO_SELECT_FUNC(n) static void servo_select_##n(bool unused) \
-{\
-    pin = servo_pins[sizeof(servo_pins) / sizeof(servo_pins[0]) - (n)];\
-}
-
-#define SERVO_SELECT_BIND(n) { .key = (uint8_t)((n) + 48), .func = servo_select_##n }
-
-SERVO_SELECT_FUNC(1)
-SERVO_SELECT_FUNC(2)
-SERVO_SELECT_FUNC(3)
-SERVO_SELECT_FUNC(4)
-SERVO_SELECT_FUNC(5)
-
-void move_servo_forward(bool pressed)
-{
-    servo_thread_change(sth, pin, pressed ? SERVO_DIRECTION_FORWARD : SERVO_DIRECTION_NONE);
-}
-
-void move_servo_backward(bool pressed)
-{
-    servo_thread_change(sth, pin, pressed ? SERVO_DIRECTION_BACKWARD : SERVO_DIRECTION_NONE);
-}
 
 struct key_bind key_binds[INPUT_KEY_BINDS] = {
     { .key =     KEY_W, .func =        motor_forward },
     { .key =     KEY_A, .func =           motor_left },
     { .key =     KEY_S, .func =       motor_backward },
     { .key =     KEY_D, .func =          motor_right },
-    { .key =     KEY_R, .func =                 stop },
-    { .key =     KEY_E, .func = move_servo_forward   },
-    { .key =     KEY_Q, .func = move_servo_backward  },
-    SERVO_SELECT_BIND(1),
-    SERVO_SELECT_BIND(2),
-    SERVO_SELECT_BIND(3),
-    SERVO_SELECT_BIND(4),
-    SERVO_SELECT_BIND(5),
 };
 
 int main(void)
 {
+    input_init();
+    struct sockaddr_in inaddr;
+    net_get_interface_addr((struct sockaddr *)&inaddr);
+    int listener = net_listener_new((struct sockaddr *)&inaddr, 1337);
+    int client = net_accept(listener);
     gpio_init();
     i2c_init();
-    input_init();
-    int listener = net_listener_new(1337);
-    int client = net_accept(listener);
     printf("Up and running\n");
     while(1) {
         uint8_t keycode = net_receive_keypress(client);
