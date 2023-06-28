59e9900995f8f7218b3e2a125eec1c4a1388d0ac
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2023-06-28 21:08:20 +0200

    main: Initialize motor subsystem, switch to keycodes

--- src/main.c
+++ src/main.c
@@ -27,7 +27,6 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include <linux/input-event-codes.h>
 
 #include "net.h"
 #include "gpio.h"
@@ -36,10 +35,10 @@
 #include "input.h"
 
 struct key_bind key_binds[INPUT_KEY_BINDS] = {
-    { .key =     KEY_W, .func =        motor_forward },
-    { .key =     KEY_A, .func =           motor_left },
-    { .key =     KEY_S, .func =       motor_backward },
-    { .key =     KEY_D, .func =          motor_right },
+    { .key =     'w', .func =        motor_forward },
+    { .key =     'a', .func =           motor_left },
+    { .key =     's', .func =       motor_backward },
+    { .key =     'd', .func =          motor_right },
 };
 
 int main(void)
@@ -50,6 +49,7 @@ int main(void)
     int listener = net_listener_new((struct sockaddr *)&inaddr, 1337);
     int client = net_accept(listener);
     gpio_init();
+    motor_init();
     i2c_init();
     printf("Up and running\n");
     while(1) {
