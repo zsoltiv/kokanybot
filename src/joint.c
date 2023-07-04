b9411a20df43cdc1f1ea188be4081fb1718583e3
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2023-07-04 23:11:01 +0200

    joint: Change pins

--- src/joint.c
+++ src/joint.c
@@ -54,7 +54,7 @@ static unsigned stepper_pins[NSTEPPERS][NPOLES] = {
     { GPIO26, GPIO19, GPIO13, GPIO6 },
     { GPIO5, SPI_SCLK, SPI_MISO, SPI_MOSI },
     { GPIO21, GPIO20, GPIO12, SPI_CE1_N },
-    { SPI_CE0_N, GPIO18, RXD0, TXD0 },
+    { SPI_CE0_N, ID_SDA, RXD0, ID_SCL },
 };
 
 static unsigned stepper_mcp_pins[NSTEPPERS][NPOLES] = {
