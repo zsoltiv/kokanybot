c6b49825df93a16181936ed140afa5b26aa4c10a
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2023-08-27 13:44:07 +0200

    gpio: Remove unused function

--- src/gpio.c
+++ src/gpio.c
@@ -26,23 +26,6 @@
 
 struct gpiod_chip *chip;
 
-static struct gpiod_line *motorpin_init(unsigned int offset)
-{
-    struct gpiod_line *line = gpiod_chip_get_line(chip, offset);
-    if(!line)
-        perror("gpiod_chip_get_line()");
-
-    if(gpiod_line_request_output(line,
-                                 GPIO_CONSUMER,
-                                 GPIO_LOW)) {
-        perror("gpiod_line_request_output()");
-    }
-
-    gpiod_line_set_value(line, GPIO_LOW);
-
-    return line;
-}
-
 void gpio_init(void)
 {
     chip = gpiod_chip_open(GPIO_CHIP_PATH);
