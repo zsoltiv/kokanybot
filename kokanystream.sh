8a7fe3d6d123dfbd4f2134de911d977b6d5487fc
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2023-12-28 21:43:10 +0100

    kokanystream: Don't buffer input frames
    God knows how many points the lack of this option cost us...

--- kokanystream.sh
+++ kokanystream.sh
@@ -6,4 +6,4 @@ port="1338"
 address="$(ip -4 -br a show $dev | sed -E 's/\s+/ /g ; s/\/[0-9]+//' | cut -d' ' -f 3)"
 echo $address
 
-ffmpeg -video_size 1280x720 -i /dev/video0 -c:v h264_v4l2m2m -b:v 3M -f mpegts "tcp://${address}:${port}?listen=1"
+ffmpeg -fflags nobuffer -video_size 1280x720 -i /dev/video0 -c:v h264_v4l2m2m -b:v 3M -f mpegts "tcp://${address}:${port}?listen=1"
