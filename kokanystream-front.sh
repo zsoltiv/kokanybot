0dd756046abd46d3d3b4c20d8cdb03ac90c8ed02
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2024-02-21 15:58:41 +0100

    Fix stream scripts
    I hate libcamera so much it's unreal

--- kokanystream-front.sh
+++ kokanystream-front.sh
@@ -2,6 +2,6 @@
 
 dev="eth0"
 port="1338"
-address="$(ip -4 -br a show $dev | sed -E 's/\s+/ /g ; s/\/[0-9]+//' | cut -d' ' -f 3)"
+address="$(arp -n -i $dev | tail -n1 | cut -d' ' -f1)"
 
-rpicam-vid --width 1296 --height 972 -n --denoise cdn_off -t 0 --listen --flush -o "tcp://${address}:${port}"
+rpicam-vid -n --denoise cdn_off -t 0 --inline --flush -o "udp://${address}:${port}"
--- kokanystream-rear.sh
+++ kokanystream-rear.sh
@@ -2,8 +2,6 @@
 
 dev="eth0"
 port="1341"
+address="$(arp -n -i $dev | tail -n1 | cut -d' ' -f1)"
 
-address="$(ip -4 -br a show $dev | sed -E 's/\s+/ /g ; s/\/[0-9]+//' | cut -d' ' -f 3)"
-echo $address
-
-ffmpeg -fflags nobuffer -f v4l2 -framerate 30 -input_format mjpeg -video_size 1280x720 -i /dev/rear-camera -c:v copy -f mjpeg "tcp://${address}:${port}?listen=1"
+ffmpeg -fflags nobuffer -f v4l2 -framerate 30 -input_format mjpeg -video_size 1280x720 -i /dev/rear-camera -c:v copy -f mjpeg "udp://${address}:${port}?buffer_size=65536&pkt_size=512"
