db5594cd0e8a6211992d54051210159dd532c4cf
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2024-02-23 15:03:40 +0100

    kokanystream-front: Use H264

--- kokanystream-front.sh
+++ kokanystream-front.sh
@@ -4,4 +4,4 @@ dev="eth0"
 port="1338"
 address="$(arp -n -i $dev | tail -n1 | cut -d' ' -f1)"
 
-rpicam-vid -n --denoise cdn_off -t 0 --inline --flush -o "udp://${address}:${port}"
+rpicam-vid --codec h264 -n --denoise cdn_off -t 0 --inline --flush -o "udp://${address}:${port}"
