6687c46130005e75a7365c70c6e4bec841fcedfa
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2024-03-06 20:00:45 +0100

    kokanystream-front, kokanystream-rear: Change the way peer IP is acquired

--- kokanystream-front.sh
+++ kokanystream-front.sh
@@ -2,6 +2,6 @@
 
 dev="eth0"
 port="1338"
-address="$(arp -n -i $dev | tail -n1 | cut -d' ' -f1)"
+address="$(nmap -sn '192.168.69.*' | grep -oE '192\.168\.69\.1[0-9]')"
 
 rpicam-vid --codec h264 -n --denoise cdn_off -t 0 --inline --flush -o "udp://${address}:${port}"
--- kokanystream-rear.sh
+++ kokanystream-rear.sh
@@ -2,6 +2,6 @@
 
 dev="eth0"
 port="1341"
-address="$(arp -n -i $dev | tail -n1 | cut -d' ' -f1)"
+address="$(nmap -sn '192.168.69.*' | grep -oE '192\.168\.69\.1[0-9]')"
 
 ffmpeg -fflags nobuffer -f v4l2 -framerate 30 -input_format mjpeg -video_size 1280x720 -i /dev/rear-camera -c:v copy -f mjpeg "udp://${address}:${port}?buffer_size=65536&pkt_size=512"
