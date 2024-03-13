f068f8d4f3dfe29028a9e5cecce6bce3b56e56f1
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2024-03-13 14:30:47 +0100

    Reconnect on any errno
    For some reason we were getting `ENXIO` instead of any of the errnos
    listed in `recv(3p)`, so just go all in

--- src/main.c
+++ src/main.c
@@ -105,7 +105,7 @@ int main(void)
     printf("Up and running\n");
     while(1) {
         uint8_t keycode = net_receive_keypress(client);
-        if(!keycode && (errno == ECONNRESET || errno == EPIPE)) {
+        if(!keycode) {
             close(client);
             client = net_accept(PORT_CTL);
         }
