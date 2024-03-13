fa05fc8758e49379e2746a4269cedce1f00e21b2
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2024-03-13 13:38:59 +0100

    kokanystream-front: Increase resolution
    This increases the accuracy of libzbar as well since it works with the
    same frames that are displayed

--- kokanystream-front.sh
+++ kokanystream-front.sh
@@ -10,4 +10,4 @@ done
 
 echo "Client ${address}"
 
-rpicam-vid --codec h264 -n --denoise cdn_off -t 0 --inline --flush -o "udp://${address}:${port}"
+rpicam-vid --codec h264 --width 1296 --height 972 -n --denoise cdn_off -t 0 --inline --flush -o "udp://${address}:${port}"
