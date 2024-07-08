8bf4d04da9895011189728a70b23591126cd26be
Zsolt Vadasz <zsolt_vadasz@protonmail.com>
2024-07-08 14:14:26 +0200

    kokanystream-rear: flip screen
    still need to fix the colours (well, the lack thereof)

--- kokanystream-rear.sh
+++ kokanystream-rear.sh
@@ -10,4 +10,4 @@ done
 
 echo "Client ${address}"
 
-ffmpeg -fflags nobuffer -f v4l2 -framerate 30 -input_format mjpeg -video_size 1280x720 -i /dev/rear-camera -c:v copy -f mjpeg "udp://${address}:${port}?buffer_size=65536&pkt_size=512"
+ffmpeg -fflags nobuffer -f v4l2 -framerate 30 -input_format mjpeg -video_size 1280x720 -i /dev/rear-camera -c:v mjpeg -f mjpeg -vf vflip "udp://${address}:${port}?buffer_size=65536&pkt_size=512"
